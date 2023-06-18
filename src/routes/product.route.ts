b2056a2bd2220894b0328a055197b7ad65966a72
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-06-18 02:13:24 -0300

    feat(product.route.ts): add post test route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -3,6 +3,7 @@ import productController from '../controllers/product.controller';
 
 const router = Router();
 
-router.post('/sort', productController.execute);
+router.post('/sort', productController.createSortedList);
+router.post('/post', productController.execute);
 
 export default router;
