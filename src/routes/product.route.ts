c0905823f56b835af90824a51a4f1f8533fc80f7
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-08-06 14:44:43 -0300

    refactor: add linting refactoring

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
-import productController from '../controllers/product.controller';
+import { Router } from "express";
+import productController from "../controllers/product.controller";
 
 const router = Router();
 
-router.post('/sort', productController.createSortedList);
-router.post('/post', productController.execute);
+router.post("/sort", productController.createSortedList);
+router.post("/", productController.testPost);
 
 export default router;
